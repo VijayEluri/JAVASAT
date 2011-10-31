08f8f8d8ada83a5398edff7f34467a0aace44998
Trevor Stevens <tstevens@me.com>
2011-10-31 19:13:54 -0400

    Remove unnecessary done flag.

--- Solver.java
+++ Solver.java
@@ -2,15 +2,11 @@ import java.util.EmptyStackException;
 
 public final class Solver {
 
-    private static boolean done = false;
-
     public static void main(final String[] args) {
         final Formula formula = new Formula(args[0]);
         try {
-            while (!done) {
-                if (formula.validSolution()) {
-                    done = true;
-                } else if (formula.getLastClauseSizeResult()) {
+            while (!formula.validSolution()) {
+                if (formula.getLastClauseSizeResult()) {
                     formula.backTrack();
                 } else {
                     formula.forwardTrack();
